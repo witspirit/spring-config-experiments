07f5c9fbbe01dc86df777cab6fd393cda00d43fc
witspirit <witspirit@gmail.com>
2016-02-07 09:59:46 +0100

    Added actuator to enable environment refresh Added exposure for profile dependent values

---
+++ config-client/src/main/java/be/witspirit/config/ConfigValues.java
@@ -0,0 +1,41 @@
+package be.witspirit.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Attempt to capture the config values in a different way
+ */
+@RefreshScope
+@Component
+public class ConfigValues {
+
+    @Value("${application.title}")
+    private String title;
+
+    @Value("${environment}")
+    private String environment;
+
+    @Value("${common.property:UNSET}")
+    private String commonProperty;
+
+    @Value("${deployment.target:UNSPECIFIED}")
+    private String deploymentTarget;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public String getCommonProperty() {
+        return commonProperty;
+    }
+
+    public String getDeploymentTarget() {
+        return deploymentTarget;
+    }
+}
--- config-client/src/main/java/be/witspirit/config/PropertyExposure.java
+++ config-client/src/main/java/be/witspirit/config/PropertyExposure.java
@@ -1,28 +1,47 @@
 package be.witspirit.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PropertyExposure {
+    private static final Logger LOG = LoggerFactory.getLogger(PropertyExposure.class);
 
     @Autowired
-    private ConfigurableEnvironment env;
+    private Environment env;
+
+    @Autowired
+    private ConfigValues configValues;
 
     @RequestMapping("/")
-    public String home() {
+    public String profiles() {
+        LOG.info("profiles");
+        return configValues.getTitle() + " on " + configValues.getDeploymentTarget();
+    }
+
+    @RequestMapping("/fromenv")
+    public String fromenv() {
+        LOG.info("fromenv");
         return env.getProperty("application.title")+" - "+env.getProperty("environment");
     }
 
     @RequestMapping("/common")
     public String common() {
+        LOG.info("common");
         return env.getProperty("common.property");
     }
 
-    @RequestMapping("/sources")
-    public String sources() {
-        return env.getPropertySources().toString();
+    @RequestMapping("/configvalues")
+    public String configValues() {
+        LOG.info("configValues");
+        return configValues.getTitle() + " - " + configValues.getEnvironment() + "(" + configValues.getCommonProperty()+")";
     }
+
 }
